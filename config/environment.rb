c4b4be8132f844749427d9fdf8b2de038dddc843
fmsmatthias <matthias.scheele06@googlemail.com>
2013-01-18 10:22:11 +0100

    status issue corrected

--- config/environment.rb
+++ config/environment.rb
@@ -6,14 +6,14 @@ SampleApp::Application.initialize!
 
 ActionMailer::Base.delivery_method = :smtp
 
-
 ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.strato.com', ENV['FMS_ADR'],
-    :domain         => 'www.biot06.com',  ENV['FMS_DOM'],
+    :address        => ENV['FMS_ADR'],
+    :domain         => ENV['FMS_DOM'],
     :port           => 587,
-    :user_name      => 'studio@biot06.com', ENV['FMS_USR'],
-    :password       => '9%wetter65x',       ENV['FMS_PWR'],
+    :user_name      => ENV['FMS_USR'],
+    :password       => ENV['FMS_PWR'],
     :authentication => :plain
 }
+
 
 
