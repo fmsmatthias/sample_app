1d0c9ec941a6a801e4a2c93ee78896522b8ccd0b
fmsmatthias <matthias.scheele06@googlemail.com>
2013-11-16 23:33:21 +0100

    deactivate welcome mail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,8 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
-      UserMailer.welcome_email(@user).deliver
+#      deactivate welcome e-mail; later use flag
+#      UserMailer.welcome_email(@user).deliver
       redirect_to @user, :flash => { :success => "Your Account has been created!" }
     else
       @title = "Sign up"
